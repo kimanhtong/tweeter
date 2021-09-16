6c953496db698780a7b536e6af38d54fc352dc2a
Anh Tong <tongkimanh@yahoo.com>
2021-09-16 15:26:33 +0000

    Make form dynamic

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,67 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+  /*for (const item of tweets) {
+    const $tweet = createTweetElement(item);
+    $('#tweets-container').append($tweet);
+  }*/
+  if (tweets.length > 0) {
+    tweets.forEach(item => {
+      const $tweet = createTweetElement(item);
+      $('#tweets-container').prepend($tweet);
+    })
+  }
+}
+
+const createTweetElement = function (tweetObj) {
+  const $tweetArticle = `
+  <article class="tweet">
+    <h4>
+      <p>
+        <img src="${tweetObj.user.avatars}"> 
+        <label> ${tweetObj.user.name} </label>
+      </p>
+      <p> ${tweetObj.user.handle} </p>
+    </h4>
+    <section class = tweeter-content>${tweetObj.content.text}</section>
+    <footer>
+      <time class="timeago"> ${timeago.format(new Date(tweetObj.created_at))}</time>
+      <p>
+        <i class="fa fa-flag"></i>
+        <i class="fa fa-heart"></i>
+        <i class="fa fa-retweet"></i>
+      </p>
+    </footer>
+  </article>`;
+  return $tweetArticle;
+};
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  renderTweets(data);
+
   $("#tweet-text").on("input", function (){
     const count = 140 - this.value.length;
     $(".counter").each (function () {
@@ -12,4 +14,22 @@ $(document).ready(function(){
       }
     });
   });
+
+  $("form").submit(function(event) {
+    console.log($("counter").textContent);
+    if ($("counter") === 140) {
+      const msg = "Your tweet cannot be empty.";
+      console.log(msg);
+      /*how to display the msg???*/
+      return false
+    }
+    if ($("counter") < 0) {
+      const msg = "Your tweet is too long.";
+      /*how to display the msg???*/
+      console.log(msg);
+      return false
+    }
+    event.preventDefault();
+  });
+  
 });
